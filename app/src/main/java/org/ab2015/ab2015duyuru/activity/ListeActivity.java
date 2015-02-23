5e80aeb2c2d6d5cc96927c69e4566e98759362df
Caner Gural <cgural@gmail.com>
2015-02-23 12:48:57 +0200

    ListeActivity'ye dummy veriler ekledik

--- app/src/main/java/org/ab2015/ab2015duyuru/activity/ListeActivity.java
+++ app/src/main/java/org/ab2015/ab2015duyuru/activity/ListeActivity.java
@@ -5,15 +5,36 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import org.ab2015.ab2015duyuru.R;
 
 public class ListeActivity extends ActionBarActivity {
 
+    ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste);
+
+        String duyurular[]={"duyuru 1", "duyuru 2",
+                "duyuru 3", "duyuru 4",
+                "duyuru 5", "duyuru 6",
+                "duyuru 7", "duyuru 8",
+                "duyuru 9", "duyuru 10",
+                "duyuru 11", "duyuru 12",
+                "duyuru 13", "duyuru 14",
+                "duyuru 15", "duyuru 16",
+        };
+
+        listView= (ListView) findViewById(R.id.listDuyurular);
+
+        ArrayAdapter<String> adapter;
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,
+                duyurular);
+        listView.setAdapter(adapter);
     }
 
 
